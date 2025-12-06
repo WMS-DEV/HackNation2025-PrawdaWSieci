a81c20a83fcfa189b07615021b30383a38423607
Marcel Dybek <92519157+MDybek@users.noreply.github.com>
2025-12-06 18:40:28 +0100

    Added hash repository (#1)
    * Added hash repository

--- backend/hacknation/src/main/java/pl/wmsdev/hacknation/service/GovValidatorService.java
+++ backend/hacknation/src/main/java/pl/wmsdev/hacknation/service/GovValidatorService.java
@@ -1,24 +1,32 @@
 package pl.wmsdev.hacknation.service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import pl.wmsdev.hacknation.entity.CheckResponse;
+import pl.wmsdev.hacknation.entity.HashEntry;
 import pl.wmsdev.hacknation.entity.PageData;
+import pl.wmsdev.hacknation.repository.HashRepository;
+import pl.wmsdev.hacknation.values.CheckResult;
 
 import javax.net.ssl.HttpsURLConnection;
 import java.net.InetAddress;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 import java.util.regex.Pattern;
 
 @Service
+@RequiredArgsConstructor
 public class GovValidatorService {
 
     private static final Pattern GOV_PL_PATTERN = Pattern.compile("^(?:[a-zA-Z0-9-]+\\.)*gov\\.pl$");
+    private final HashRepository hashRepository;
 
     public CheckResponse validatePage(PageData pageData) {
         List<String> reasons = new ArrayList<>();
         boolean isValid = true;
+        UUID validationId = UUID.randomUUID();
 
         try {
             URL url = new URL(pageData.url());
@@ -59,6 +67,14 @@ public class GovValidatorService {
             reasons.add("Invalid URL: " + e.getMessage());
         }
 
-        return new CheckResponse(isValid, reasons);
+        hashRepository.save(HashEntry.builder()
+                .id(validationId)
+                .originalUrl(pageData.url())
+                .serverIp(pageData.serverIp())
+                .timestamp(pageData.timestamp())
+                .result(isValid ? CheckResult.VALID : CheckResult.INVALID)
+                .build());
+
+        return new CheckResponse(validationId);
     }
 }
