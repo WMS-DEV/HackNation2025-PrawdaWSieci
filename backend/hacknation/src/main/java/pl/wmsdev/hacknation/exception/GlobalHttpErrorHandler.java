2aa5f1d3d61e38fb01f17f3318d6bcc1d21ca2b7
MDybek <dybek8780@gmail.com>
2025-12-07 00:29:26 +0100

    Added better validation

---
+++ backend/hacknation/src/main/java/pl/wmsdev/hacknation/exception/GlobalHttpErrorHandler.java
@@ -0,0 +1,29 @@
+package pl.wmsdev.hacknation.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+public class GlobalHttpErrorHandler {
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(RuntimeException.class)
+    public ErrorResponse handleRuntimeException(RuntimeException ex) {
+        return new ErrorResponse(List.of(ex.getMessage()));
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ErrorResponse handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        List<String> errors = ex.getBindingResult().getFieldErrors().stream()
+                .map(error -> error.getField() + ": " + error.getDefaultMessage())
+                .collect(Collectors.toList());
+        return new ErrorResponse(errors);
+    }
+}
